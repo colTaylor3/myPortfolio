1ecfe87b9a505a7800e7fd7417b250d1344aa96d
colTaylor3 <80934161+colTaylor3@users.noreply.github.com>
2021-03-18 21:47:16 -0500

    Add files via upload

---
+++ Current Date and Time.js
@@ -0,0 +1,11 @@
+var today = new Date();
+var dayOfTheMonth = today.getDate();
+
+var mm = today.getMonth() + 1;
+var yy = today.getFullYear();
+
+
+today = mm+ '-' + dayOfTheMonth + '-' + yy;
+console.log(today);
+
+
